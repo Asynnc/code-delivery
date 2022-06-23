de5ee35ad1206eadbd5a7e5c9c170813afc5b7a8
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2022-06-23 17:33:58 -0300

    fix: conteiners kafka, zookeeper and cp-entreprise-control-center now run on apple sillicon

--- Simulator/main.go
+++ Simulator/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
 	route2 "gitlab.com/Tonybsilva-dev/code-delivery-simulator/application/route"
 )
 
-func main(){
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
 	route := route2.Route{
-		ID: "1",
+		ID:       "1",
 		ClientID: "1",
 	}
 	route.LoadPositions()
